f92a3cead0baf4b13f747bd7d484b59c8c0e0efe
MoisesCordones <gonmoises449@gmail.com>
2023-07-29 13:02:22 -0400

    RisxPert

---
+++ RisxPert/Form1.Designer.cs
@@ -0,0 +1,412 @@
+﻿namespace PPM
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            tabPage4 = new TabPage();
+            tabPage3 = new TabPage();
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            dataGridView2 = new DataGridView();
+            button6 = new Button();
+            button7 = new Button();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            tabPage4.SuspendLayout();
+            tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            tabControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.ImageScalingSize = new Size(20, 20);
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // tabPage4
+            // 
+            tabPage4.Controls.Add(label5);
+            tabPage4.Controls.Add(label6);
+            tabPage4.Controls.Add(label7);
+            tabPage4.Controls.Add(label8);
+            tabPage4.Controls.Add(textBox5);
+            tabPage4.Controls.Add(textBox6);
+            tabPage4.Controls.Add(textBox7);
+            tabPage4.Controls.Add(textBox8);
+            tabPage4.Controls.Add(button6);
+            tabPage4.Controls.Add(button7);
+            tabPage4.Controls.Add(dataGridView2);
+            tabPage4.Location = new Point(4, 33);
+            tabPage4.Name = "tabPage4";
+            tabPage4.Padding = new Padding(3);
+            tabPage4.Size = new Size(1299, 598);
+            tabPage4.TabIndex = 3;
+            tabPage4.Text = "FASE 2";
+            tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            tabPage3.Controls.Add(label4);
+            tabPage3.Controls.Add(label3);
+            tabPage3.Controls.Add(label2);
+            tabPage3.Controls.Add(label1);
+            tabPage3.Controls.Add(textBox4);
+            tabPage3.Controls.Add(textBox3);
+            tabPage3.Controls.Add(textBox2);
+            tabPage3.Controls.Add(textBox1);
+            tabPage3.Controls.Add(button3);
+            tabPage3.Controls.Add(button2);
+            tabPage3.Controls.Add(button1);
+            tabPage3.Controls.Add(dataGridView1);
+            tabPage3.Location = new Point(4, 33);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Padding = new Padding(3);
+            tabPage3.Size = new Size(1299, 598);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "FASE 1";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(6, 234);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(1269, 347);
+            dataGridView1.TabIndex = 2;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(314, 188);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Save";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(173, 188);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 1;
+            button2.Text = "Delete";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(38, 188);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 3;
+            button3.Text = "Add";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(173, 28);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 33);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(173, 72);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 33);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(173, 111);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 33);
+            textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(1113, 31);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(125, 33);
+            textBox4.TabIndex = 8;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.MintCream;
+            label1.Font = new Font("3ds", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.ActiveCaptionText;
+            label1.ImageAlign = ContentAlignment.BottomCenter;
+            label1.Location = new Point(21, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(126, 33);
+            label1.TabIndex = 9;
+            label1.Text = "Nombre:";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("3ds", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(21, 72);
+            label2.Name = "label2";
+            label2.Size = new Size(111, 33);
+            label2.TabIndex = 10;
+            label2.Text = "Asesor:";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("3ds", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(21, 115);
+            label3.Name = "label3";
+            label3.Size = new Size(131, 33);
+            label3.TabIndex = 11;
+            label3.Text = "Analista:";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("3ds", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(982, 28);
+            label4.Name = "label4";
+            label4.Size = new Size(97, 33);
+            label4.TabIndex = 12;
+            label4.Text = "Fecha:";
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage3);
+            tabControl1.Controls.Add(tabPage4);
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Font = new Font("3ds", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            tabControl1.Location = new Point(12, 12);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(1307, 635);
+            tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Location = new Point(4, 33);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(1299, 598);
+            tabPage1.TabIndex = 4;
+            tabPage1.Text = "FASE 3";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 33);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(1299, 598);
+            tabPage2.TabIndex = 5;
+            tabPage2.Text = "FASE 4";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(15, 245);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowHeadersWidth = 51;
+            dataGridView2.RowTemplate.Height = 29;
+            dataGridView2.Size = new Size(1269, 347);
+            dataGridView2.TabIndex = 3;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(29, 191);
+            button6.Name = "button6";
+            button6.Size = new Size(94, 29);
+            button6.TabIndex = 5;
+            button6.Text = "Delete";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(156, 191);
+            button7.Name = "button7";
+            button7.Size = new Size(94, 29);
+            button7.TabIndex = 4;
+            button7.Text = "Save";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("3ds", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(990, 24);
+            label5.Name = "label5";
+            label5.Size = new Size(97, 33);
+            label5.TabIndex = 20;
+            label5.Text = "Fecha:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("3ds", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(29, 111);
+            label6.Name = "label6";
+            label6.Size = new Size(131, 33);
+            label6.TabIndex = 19;
+            label6.Text = "Analista:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("3ds", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(29, 68);
+            label7.Name = "label7";
+            label7.Size = new Size(111, 33);
+            label7.TabIndex = 18;
+            label7.Text = "Asesor:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = Color.MintCream;
+            label8.Font = new Font("3ds", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.ForeColor = SystemColors.ActiveCaptionText;
+            label8.ImageAlign = ContentAlignment.BottomCenter;
+            label8.Location = new Point(29, 20);
+            label8.Name = "label8";
+            label8.Size = new Size(126, 33);
+            label8.TabIndex = 17;
+            label8.Text = "Nombre:";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(1121, 27);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(125, 33);
+            textBox5.TabIndex = 16;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(181, 107);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(125, 33);
+            textBox6.TabIndex = 15;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(181, 68);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(125, 33);
+            textBox7.TabIndex = 14;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(181, 24);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(125, 33);
+            textBox8.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Green;
+            ClientSize = new Size(1331, 779);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "Form1";
+            tabPage4.ResumeLayout(false);
+            tabPage4.PerformLayout();
+            tabPage3.ResumeLayout(false);
+            tabPage3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            tabControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private ContextMenuStrip contextMenuStrip1;
+        private TabPage tabPage4;
+        private TabPage tabPage3;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private DataGridView dataGridView1;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private DataGridView dataGridView2;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private Button button6;
+        private Button button7;
+    }
+}
---
+++ RisxPert/Form1.cs
@@ -0,0 +1,35 @@
+namespace PPM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
